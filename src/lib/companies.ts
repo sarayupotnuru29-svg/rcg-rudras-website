252625e18a371445704a683f4a68aa7a7f7417bc
sarayupotnuru29-svg <sarayupotnuru29@gmail.com>
2026-02-05 15:46:05 +0530

    upadted

--- src/lib/companies.ts
+++ src/lib/companies.ts
@@ -4,6 +4,24 @@ import amazonLogo from "@/assets/companies/amazon.png";
 import microsoftLogo from "@/assets/companies/microsoft.png";
 import wiproLogo from "@/assets/companies/wipro.png";
 import tcsLogo from "@/assets/companies/tcs.png";
+import Ericssionlogo from "@/assets/companies/ericssion.png";
+import Saintgobainlogo from "@/assets/companies/saint gobain.png";
+import sonylogo from "@/assets/companies/sony.webp";
+import inivitlogo from "@/assets/companies/inivit.png";
+import gartnerlogo from "@/assets/companies/gartner.png";
+import Artificiallogo from "@/assets/companies/artificial_penetration_logo.jpg";
+import adobelogo from "@/assets/companies/adobe.webp";
+import ciscologo from "@/assets/companies/Cisco.jpg";
+import zslogo from "@/assets/companies/zs.webp";
+import EYlogo from "@/assets/companies/EY.png";
+import amdocslogo from "@/assets/companies/amdocs.png";
+import mysigmalogo from "@/assets/companies/Mu_sigma_logo.jpg";
+import FAIlogo from "@/assets/companies/FAI.png";
+import yokogawalogo from "@/assets/companies/yokogalwa.png";
+import societelogo from "@/assets/companies/Societe-Generale-Logo.jpg";
+import samsunglogo from "@/assets/companies/samsung.png";
+import DHLlogo from "@/assets/companies/DHL.png";
+import fedexlogo from "@/assets/companies/fedEx.png";
 import infosysLogo from "@/assets/companies/infosys.png";
 import hclLogo from "@/assets/companies/hcl.png";
 import accentureLogo from "@/assets/companies/accenture.png";
@@ -23,6 +41,24 @@ export const companies: Company[] = [
   { name: "Microsoft", logo: microsoftLogo },
   { name: "Wipro", logo: wiproLogo },
   { name: "TCS", logo: tcsLogo },
+  { name: "Ericssion", logo: Ericssionlogo },
+  { name: "SaintGobain", logo: Saintgobainlogo},
+  { name: "Sony", logo: sonylogo },
+  { name: "Inivit", logo: inivitlogo },
+  { name: "Gartner", logo: gartnerlogo },
+  { name: "Artificial Penetration", logo: Artificiallogo },
+  { name: "adobe", logo: adobelogo },
+  { name: "cisco", logo: ciscologo },
+  { name: "ZS", logo: zslogo },
+  { name: "EY", logo: EYlogo },
+  { name: "Amdocs", logo: amdocslogo },
+  { name: "my sigma", logo: mysigmalogo },
+  { name: "FAI", logo: FAIlogo },
+  { name: "Yokogawa", logo: yokogawalogo },
+  { name: "Societe generale", logo: societelogo },
+  { name: "Samsung", logo: samsunglogo },
+  { name: "DHL", logo: DHLlogo },
+  { name: "FedEx", logo: fedexlogo },
   { name: "Infosys", logo: infosysLogo },
   { name: "HCL", logo: hclLogo },
   { name: "Tech Mahindra", logo: techMahindraLogo },
